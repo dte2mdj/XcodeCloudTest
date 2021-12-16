ef3ad6c35fd284bce49a381e9d92f7e710c5b0af
xwg <dte2mdj@126.com>
2021-12-16 11:32:54 +0800

    初始化

---
+++ XcodeCloudTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeCloudTest
+//
+//  Created by xwg on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
